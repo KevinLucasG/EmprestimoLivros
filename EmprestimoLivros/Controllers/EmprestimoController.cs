e8e7a9ad63c6f27c4feb6a6c84ce21394984702e
Kevin <kevin.lucassg@gmail.com>
2024-05-20 23:20:17 -0300

    Escopo Inicial

---
+++ EmprestimoLivros/Controllers/EmprestimoController.cs
@@ -0,0 +1,20 @@
+﻿using EmprestimoLivros.Data;
+using EmprestimoLivros.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EmprestimoLivros.Controllers
+{
+    public class EmprestimoController : Controller
+    {
+        readonly private ApplicationDbContext _db;
+        public EmprestimoController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            IEnumerable<EmpretismoModel> emprestimos = _db.Empretismos;
+            return View(emprestimos);
+        }
+    }
+}
